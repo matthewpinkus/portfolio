66e56c23d1683dd6ff3414023d49e22f7fc05276
kaildrai <matthew.pinkus3@gmail.com>
2023-05-16 16:00:51 +1000

    (tiles) added about tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import AboutTile from './components/AboutTile';
 import StickyAboutSection from './components/StickyAboutSection';
 
 function App() {
   return (
     <div className='p-12 bg-primary_dark'>
-      <StickyAboutSection />
+      <div className='flex justify-center'>
+        <StickyAboutSection />
+        <AboutTile />
+
+      </div>
     </div>
     
   )
---
+++ src/components/AboutTile.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AboutTile() {
+  return (
+    <div>
+      <h1 className="text-5xl text-heading_dark">ABOUT TILE</h1> 
+    </div>
+  )
+}
