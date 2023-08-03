6c2b4855cae980bea700bb12f0d5f2d68c8c6f5b
kaildrai <matthew.pinkus3@gmail.com>
2023-08-03 15:10:55 +1000

    footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="section md:pb-36 text-paragraph_dark_low">
+    <div className="section pb-8 max-md:pl-8 md:py-16 text-paragraph_dark_low">
       <span>
         Inspired by{" "}
         <a className="link" target="_blank" href="https://brittanychiang.com/">
